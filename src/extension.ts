93670502e73ad1dfbb39f3b9285d0e3d40878628
Radu Mereuta <headness13@gmail.com>
2023-08-22 21:44:32 +0300

    Wait for the result

--- src/extension.ts
+++ src/extension.ts
@@ -12,9 +12,15 @@ import { CodelensProvider } from './CodelensProvider';
 import * as path from "path";
 import fetch from 'cross-fetch';
 
-export const ercxRootSet = new Set<vscode.TestItem>();
-export const ercxTestsAPI = new Map<string, ERCxTestAPI>();
-export const ercxAPIUri:string = vscode.workspace.getConfiguration('ercx').get('ercxAPIUri') ?? "https://ercx.runtimeverification.com/api/v1/";
+const ercxRootSet = new Set<vscode.TestItem>();
+const ercxTestsAPI = new Map<string, ERCxTestAPI>();
+const ercxAPIUri:string = vscode.workspace.getConfiguration('ercx').get('ercxAPIUri') ?? "https://ercx.runtimeverification.com/api/v1/";
+const outputChannel = vscode.window.createOutputChannel('ERCx');
+
+function log(msg?:any) {
+	console.log(msg);
+	outputChannel.appendLine(msg);
+}
 
 export async function activate(context: vscode.ExtensionContext) {
 	const codelensProvider = new CodelensProvider();
@@ -25,7 +31,7 @@ export async function activate(context: vscode.ExtensionContext) {
 
 	vscode.commands.registerCommand("ercx.codelensAction", (document: vscode.TextDocument, ctrctName: string, range:vscode.Range) => {
 		//vscode.window.showInformationMessage(`CodeLens action clicked with args=${fileName} ${ctrctName} ${range}`);
-		console.log(`CodeLens action clicked with args=${document.uri} ${ctrctName} ${range}`);
+		log(`CodeLens action clicked with args=${document.uri} ${ctrctName} ${range}`);
 		addERCxTestsAPI(ctrl, document, range);
 	});
 
@@ -46,8 +52,6 @@ export async function activate(context: vscode.ExtensionContext) {
 		const resultFile:string = queue.at(0)?.uri?.fsPath.toString().substring(0, queue.at(0)?.uri?.fsPath.toString().lastIndexOf(".")) + ".result.json";
 		const filePath:string = queue.at(0)?.uri?.fsPath.toString() + "";
 		const fileContent:string = readFileSync(filePath, "utf8");
-		//TODO: const ercxRunRes:string = execERCxAndGetResult(queue.at(0)?.uri?.fsPath.toString() ?? "", "MyToken");
-		let res = JSON.parse("{}"); //JSON.parse(readFileSync(resultFile, "utf8"));
 
 		const apiRes = fetch(ercxAPIUri + "reports", {
 			method: "POST",
@@ -59,64 +63,33 @@ export async function activate(context: vscode.ExtensionContext) {
 					"content": fileContent,
 					"path": queue.at(0)?.uri?.fsPath.toString()
 				},
-				"tokenClass": "MyToken"
+				"tokenClass": "MyToken" //TODO: find the token name
 			})
 		}).then(response => {
-			console.log(response.body);
 			if (response.ok) {
 				const x = response.json();
 				x.then(body => {
 					if (body['status'] == "DONE") {
 						const report = JSON.parse(body['json']);
-						console.log("report: " + report);
-						res = report;
-						// For every test that was queued, try to run it. Call run.passed() or run.failed().
-						// The `TestMessage` can contain extra information, like a failing location or
-						// a diff output. But here we'll just give it a textual message.
-						while (queue.length > 0 && !cancellation.isCancellationRequested) {
-							const test = queue.pop()!;
-
-							// Skip tests the user asked to exclude
-							if (request.exclude?.includes(test)) {
-								continue;
-							}
-							console.log("test:" + test.label);
-							if (test.children.size != 0) {
-								test.children.forEach(test => queue.push(test));
-							} else {
-								for (const [k, v] of Object.entries(res)) {
-									//console.log(k + " " + v);
-									for (const [k1, v1] of Object.entries((v as any)['test_results'])) {
-										const tresult = v1 as any;
-										//console.log(k1 + " " + tresult);
-										if (k1.startsWith(test.id + "(")) {
-											if (tresult['status'] == "Success") {
-												run.passed(test, 1);
-											} else {
-												const feedback:string = ercxTestsAPI.get(test.id)?.feedback ?? "error";
-												const expected:string = ercxTestsAPI.get(test.id)?.property ?? "error";
-												run.failed(test, vscode.TestMessage.diff(new vscode.MarkdownString(feedback), expected, feedback), 1);
-												//run.failed(test, new vscode.TestMessage(new vscode.MarkdownString(feedback)), 1); // this can take Markdown as input
-											}
-										}
-									}
-								}
-							}
-						}
+						testingDone(report, run, request, queue, cancellation);
+						run.end();
+					} else if (body['status'] == "RUNNING") {
+						log("running...");
+						new Promise(resolve => setTimeout(resolve, 1000)).then(rr => 
+							testingRunning(body['id'] as string, run, request, queue, cancellation)
+						);
 					} else {
-						console.log("Status: " + body['status']);
+						log("Status: " + body['status']);
 					}
 					// Make sure to end the run after all tests have been executed:
-					run.end();
 				});
 			} else {
-				console.log("response !OK");
+				log("response !OK");
 				// Make sure to end the run after all tests have been executed:
 				run.end();
 			}
-		}, err => console.log("Report error: " + err))
-		.catch(error => console.log("API error: " + error));
-		console.log("Interpreting result" + res);
+		}, err => log("Report error: " + err))
+		.catch(error => log("API error: " + error));
 	};
 
 	ctrl.refreshHandler = async () => {
@@ -124,6 +97,78 @@ export async function activate(context: vscode.ExtensionContext) {
 	};
 
 	ctrl.createRunProfile('Run Tests', vscode.TestRunProfileKind.Run, runHandler, true, undefined, true);
+}
+
+function testingRunning(id:string, run:vscode.TestRun, request: vscode.TestRunRequest, queue: vscode.TestItem[], cancellation: vscode.CancellationToken) {
+	if (!cancellation.isCancellationRequested) {
+		
+		const apiRes = fetch(ercxAPIUri + "reports/" + id + "?fields=text%2Cjson", {
+			method: "GET",
+			headers: { "Content-Type": "application/json" }
+		}).then(response => {
+			if (response.ok) {
+				const x = response.json();
+				x.then(body => {
+					if (body['status'] == "DONE") {
+						const report = JSON.parse(body['json']);
+						testingDone(report, run, request, queue, cancellation);
+						run.end();
+					} else if (body['status'] == "RUNNING") {
+						log("running...");
+						new Promise(resolve => setTimeout(resolve, 1000)).then(rr => 
+							testingRunning(body['id'] as string, run, request, queue, cancellation)
+						);
+					} else {
+						log("Status: " + body['status']);
+					}
+					// Make sure to end the run after all tests have been executed:
+				});
+			} else {
+				log("response !OK");
+				// Make sure to end the run after all tests have been executed:
+				run.end();
+			}
+		}, err => log("Report error: " + err))
+		.catch(error => log("API error: " + error));
+	} else {
+		run.end();
+		log("User requested to end.");
+	}
+}
+
+function testingDone(res:any, run:vscode.TestRun, request: vscode.TestRunRequest, queue: vscode.TestItem[], cancellation: vscode.CancellationToken) {
+	// For every test that was queued, try to run it. Call run.passed() or run.failed().
+	while (queue.length > 0 && !cancellation.isCancellationRequested) {
+		const test = queue.pop()!;
+
+		// Skip tests the user asked to exclude
+		if (request.exclude?.includes(test)) {
+			continue;
+		}
+		//log("test:" + test.label);
+		if (test.children.size != 0) {
+			test.children.forEach(test => queue.push(test));
+		} else {
+			for (const [k, v] of Object.entries(res)) {
+				//log(k + " " + v);
+				for (const [k1, v1] of Object.entries((v as any)['test_results'])) {
+					const tresult = v1 as any;
+					//log(k1 + " " + tresult);
+					if (k1.startsWith(test.id + "(")) {
+						if (tresult['status'] == "Success") {
+							run.passed(test, 1);
+						} else {
+							const feedback:string = ercxTestsAPI.get(test.id)?.feedback ?? "error";
+							const expected:string = ercxTestsAPI.get(test.id)?.property ?? "error";
+							run.failed(test, vscode.TestMessage.diff(new vscode.MarkdownString(feedback), expected, feedback), 1);
+							//run.failed(test, new vscode.TestMessage(new vscode.MarkdownString(feedback)), 1); // this can take Markdown as input
+						}
+					}
+				}
+			}
+		}
+	}
+	log("Testing done");
 }
 
 function getWorkspaceTestPatterns() {
@@ -162,25 +207,25 @@ function addERCxTestsAPI(controller: vscode.TestController, document: vscode.Tex
 			method: "GET",
 			headers: { "Content-Type": "application/json"}
 		}).then(response => {
-			console.log(response.body);
+			log(response.body);
 			if (response.ok) {
 				const x = response.json();
 				x.then(body => {
 					const apitsts = body as ERCxTestAPI[];
 					for (const tst of apitsts)
 						ercxTestsAPI.set(tst.name, tst);
-					console.log(ercxTestsAPI);
+					log(ercxTestsAPI);
 					addERCxTestsAPI2(controller, document, range);
 				});
 			}
-		}, err => console.log("Property tests fetch error: " + err))
-		.catch(error => console.log("API error: " + error));
+		}, err => log("Property tests fetch error: " + err))
+		.catch(error => log("API error: " + error));
 }
 
 // construct TestItems based on the test list from the API
 function addERCxTestsAPI2(controller: vscode.TestController, document: vscode.TextDocument, range:vscode.Range) {
 	const docTokens = getSolidityTokenLoc(document);
-	console.log("docTokens:" + docTokens);
+	log("docTokens:" + docTokens);
 
 	const ercxRoot = controller.createTestItem("ERCx", document.uri.path.split('/').pop()! + " - ERC20 Tests", document.uri);
 	ercxRootSet.add(ercxRoot);
@@ -223,7 +268,7 @@ class ERCxTestAPI {
 
 function getSolidityTokenLoc(document:vscode.TextDocument):Map<string, vscode.Range> {
     const fileAst = JSON.parse(execSync(`solc --ast-compact-json ${document.uri.path}`).toString().split(new RegExp(`======= \\S+ =======`))[1]);
-	console.log(fileAst);
+	log(fileAst);
 	const tokenLoc = new Map<string, vscode.Range>();
 	getSolidityTokenLoc2(document, fileAst, tokenLoc);
 	return tokenLoc;
@@ -240,7 +285,7 @@ function getSolidityTokenLoc2(document:vscode.TextDocument, jsonObj:any, tokenLo
 		tokenLoc.set(name, range);
 	}
 	for (const [k, v] of Object.entries(jsonObj)) {
-		//console.log(typeof v);
+		//log(typeof v);
 		if (Array.isArray(v)) {
 			v.forEach((child:any) => getSolidityTokenLoc2(document, child, tokenLoc));
 		} else if (typeof v === 'object') {
