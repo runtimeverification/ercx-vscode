7847e87f1222d0cfdc6183f801a0c93e8c6c07e6
Radu Mereuta <headness13@gmail.com>
2023-09-12 20:54:53 +0300

    Revert some formatting
    and some code cleanup

--- src/CodelensProvider.ts
+++ src/CodelensProvider.ts
@@ -4,71 +4,57 @@ import * as vscode from 'vscode';
  * CodelensProvider
  */
 export class CodelensProvider implements vscode.CodeLensProvider {
-  private codeLenses: vscode.CodeLens[] = [];
-  private regex: RegExp;
-  private _onDidChangeCodeLenses: vscode.EventEmitter<void> =
-    new vscode.EventEmitter<void>();
-  public readonly onDidChangeCodeLenses: vscode.Event<void> =
-    this._onDidChangeCodeLenses.event;
+    private codeLenses: vscode.CodeLens[] = [];
+    private regex: RegExp;
+    private _onDidChangeCodeLenses: vscode.EventEmitter<void> =
+        new vscode.EventEmitter<void>();
+    public readonly onDidChangeCodeLenses: vscode.Event<void> =
+        this._onDidChangeCodeLenses.event;
 
-  constructor() {
-    this.regex = /contract\s+(\S+)/g;
+    constructor() {
+        this.regex = /contract\s+(\S+)/g;
 
-    vscode.workspace.onDidChangeConfiguration((_) => {
-      this._onDidChangeCodeLenses.fire();
-    });
-  }
-
-  public provideCodeLenses(
-    document: vscode.TextDocument,
-    token: vscode.CancellationToken,
-  ): vscode.CodeLens[] | Thenable<vscode.CodeLens[]> {
-    if (
-      vscode.workspace
-        .getConfiguration('codelens-sample')
-        .get('enableCodeLens', true)
-    ) {
-      this.codeLenses = [];
-      const regex = new RegExp(this.regex);
-      const text = document.getText();
-      let matches;
-      while ((matches = regex.exec(text)) !== null) {
-        const line = document.lineAt(document.positionAt(matches.index).line);
-        const indexOf = line.text.indexOf(matches[1]);
-        const position = new vscode.Position(line.lineNumber, indexOf);
-        const range = document.getWordRangeAtPosition(
-          position,
-          new RegExp(this.regex),
-        );
-        if (range) {
-          this.codeLenses.push(
-            new vscode.CodeLens(range, {
-              title: 'ERCx generate tests',
-              tooltip: 'Generate and run tests for this contract',
-              command: 'ercx.codelensAction',
-              arguments: [document, matches[1], range],
-            }),
-          );
-        }
-      }
-      return this.codeLenses;
+        vscode.workspace.onDidChangeConfiguration((_) => {
+            this._onDidChangeCodeLenses.fire();
+        });
     }
-    return [];
-  }
 
-  public resolveCodeLens(
-    codeLens: vscode.CodeLens,
-    token: vscode.CancellationToken,
-  ) {
-    /*if (vscode.workspace.getConfiguration("codelens-sample").get("enableCodeLens", true)) {
-			codeLens.command = {
-				title: "ERCx run tests",
-				tooltip: "Generate and run tests for this contract",
-				command: "codelens-sample.codelensAction",
-				arguments: ["Argument 1", false]
-			};
-			return codeLens;
-		}*/
-    return null;
-  }
+    public provideCodeLenses(
+        document: vscode.TextDocument,
+        token: vscode.CancellationToken,
+    ): vscode.CodeLens[] | Thenable<vscode.CodeLens[]> {
+        if (
+            vscode.workspace
+                .getConfiguration('ercx')
+                .get('enableCodeLens', true)
+        ) {
+            this.codeLenses = [];
+            const regex = new RegExp(this.regex);
+            const text = document.getText();
+            let matches;
+            while ((matches = regex.exec(text)) !== null) {
+                const line = document.lineAt(
+                    document.positionAt(matches.index).line,
+                );
+                const indexOf = line.text.indexOf(matches[1]);
+                const position = new vscode.Position(line.lineNumber, indexOf);
+                const range = document.getWordRangeAtPosition(
+                    position,
+                    new RegExp(this.regex),
+                );
+                if (range) {
+                    this.codeLenses.push(
+                        new vscode.CodeLens(range, {
+                            title: 'ERCx generate tests',
+                            tooltip: 'Generate and run tests for this contract',
+                            command: 'ercx.codelensAction',
+                            arguments: [document, matches[1], range],
+                        }),
+                    );
+                }
+            }
+            return this.codeLenses;
+        }
+        return [];
+    }
 }
