c9594bc09c6369bdbe36fa05d749b99ad12a1da2
Yiyi Wang <yiyi.wang@runtimeverification.com>
2023-09-12 09:41:31 +0800

    feat: Switched to yarn. Added eslint, prettier, and husky

--- README.md
+++ README.md
@@ -13,6 +13,6 @@ At this time, we only accept single files.
 
 - Run `npm install` in terminal to install dependencies
 - Run the `Run Extension` target in the Debug View (`F5`). This will:
-	- Start a task `npm: watch` to compile the code
-	- Run the extension in a new VS Code window
+  - Start a task `npm: watch` to compile the code
+  - Run the extension in a new VS Code window
 - Open a `*.sol` file containing the given content
--- src/CodelensProvider.ts
+++ src/CodelensProvider.ts
@@ -4,48 +4,63 @@ import * as vscode from 'vscode';
  * CodelensProvider
  */
 export class CodelensProvider implements vscode.CodeLensProvider {
+  private codeLenses: vscode.CodeLens[] = [];
+  private regex: RegExp;
+  private _onDidChangeCodeLenses: vscode.EventEmitter<void> =
+    new vscode.EventEmitter<void>();
+  public readonly onDidChangeCodeLenses: vscode.Event<void> =
+    this._onDidChangeCodeLenses.event;
 
-	private codeLenses: vscode.CodeLens[] = [];
-	private regex: RegExp;
-	private _onDidChangeCodeLenses: vscode.EventEmitter<void> = new vscode.EventEmitter<void>();
-	public readonly onDidChangeCodeLenses: vscode.Event<void> = this._onDidChangeCodeLenses.event;
+  constructor() {
+    this.regex = /contract\s+(\S+)/g;
 
-	constructor() {
-		this.regex = /contract\s+(\S+)/g;
+    vscode.workspace.onDidChangeConfiguration((_) => {
+      this._onDidChangeCodeLenses.fire();
+    });
+  }
 
-		vscode.workspace.onDidChangeConfiguration((_) => {
-			this._onDidChangeCodeLenses.fire();
-		});
-	}
+  public provideCodeLenses(
+    document: vscode.TextDocument,
+    token: vscode.CancellationToken,
+  ): vscode.CodeLens[] | Thenable<vscode.CodeLens[]> {
+    if (
+      vscode.workspace
+        .getConfiguration('codelens-sample')
+        .get('enableCodeLens', true)
+    ) {
+      this.codeLenses = [];
+      const regex = new RegExp(this.regex);
+      const text = document.getText();
+      let matches;
+      while ((matches = regex.exec(text)) !== null) {
+        const line = document.lineAt(document.positionAt(matches.index).line);
+        const indexOf = line.text.indexOf(matches[1]);
+        const position = new vscode.Position(line.lineNumber, indexOf);
+        const range = document.getWordRangeAtPosition(
+          position,
+          new RegExp(this.regex),
+        );
+        if (range) {
+          this.codeLenses.push(
+            new vscode.CodeLens(range, {
+              title: 'ERCx generate tests',
+              tooltip: 'Generate and run tests for this contract',
+              command: 'ercx.codelensAction',
+              arguments: [document, matches[1], range],
+            }),
+          );
+        }
+      }
+      return this.codeLenses;
+    }
+    return [];
+  }
 
-	public provideCodeLenses(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.CodeLens[] | Thenable<vscode.CodeLens[]> {
-
-		if (vscode.workspace.getConfiguration("codelens-sample").get("enableCodeLens", true)) {
-			this.codeLenses = [];
-			const regex = new RegExp(this.regex);
-			const text = document.getText();
-			let matches;
-			while ((matches = regex.exec(text)) !== null) {
-				const line = document.lineAt(document.positionAt(matches.index).line);
-				const indexOf = line.text.indexOf(matches[1]);
-				const position = new vscode.Position(line.lineNumber, indexOf);
-				const range = document.getWordRangeAtPosition(position, new RegExp(this.regex));
-				if (range) {
-					this.codeLenses.push(new vscode.CodeLens(range, {
-						title: "ERCx generate tests",
-						tooltip: "Generate and run tests for this contract",
-						command: "ercx.codelensAction",
-						arguments: [document, matches[1], range]
-					}));
-				}
-			}
-			return this.codeLenses;
-		}
-		return [];
-	}
-
-	public resolveCodeLens(codeLens: vscode.CodeLens, token: vscode.CancellationToken) {
-		/*if (vscode.workspace.getConfiguration("codelens-sample").get("enableCodeLens", true)) {
+  public resolveCodeLens(
+    codeLens: vscode.CodeLens,
+    token: vscode.CancellationToken,
+  ) {
+    /*if (vscode.workspace.getConfiguration("codelens-sample").get("enableCodeLens", true)) {
 			codeLens.command = {
 				title: "ERCx run tests",
 				tooltip: "Generate and run tests for this contract",
@@ -54,7 +69,6 @@ export class CodelensProvider implements vscode.CodeLensProvider {
 			};
 			return codeLens;
 		}*/
-		return null;
-	}
+    return null;
+  }
 }
-
